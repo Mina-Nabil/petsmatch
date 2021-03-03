27a097af54ae35c8e2e636447f19b38a97653d50
michaelshabib <63905853+michaelshabib@users.noreply.github.com>
2021-03-03 13:45:17 +0200

    new PetMatchSingleScreen

--- lib/ui/profile/ProfileScreen.dart
+++ lib/ui/profile/ProfileScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:petmatch/ui/profile/widgets/ProfileCover.dart';
 import 'package:petmatch/widgets/feed/NewPostWidget.dart';
-import 'package:petmatch/widgets/screens/PetMatchSingleScreen.dart';
+import 'package:petmatch/widgets/screens/PetMatchSingleScreen2.dart';
 
 
 class ProfileScreen extends StatelessWidget {
@@ -13,33 +13,20 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     
 
-    
     return PetMatchSingleScreen(
       backArrow: true,
-      body: SingleChildScrollView(
-        child: Column(mainAxisSize: MainAxisSize.max,
-          children: [
-            //cover
-            ProfileCover(),
-          ClipRRect(
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
-            child: Container(
-              width: double.infinity,
-              constraints: BoxConstraints(
-                minHeight: MediaQuery.of(context).size.height/2,
-              ),
-              color: Colors.grey[200],
-              child: Column(
-                children: [
-                  NewPostWidget(),
-                ],
-              )
-            ),
-          ),
-            
-          ],
-        ),
-      )
+      header: ProfileCover(),
+      body: Column(
+        children: [
+          NewPostWidget(),
+          Text("Michael"),
+          Text("Samy"),
+          Text("Samy"),
+          Text("Samy"),
+          Text("Samy"),
+
+        ],
+      ),
     );
-  }
+      }
 }
