1d69b85d9f86bd47c6582d212211decae2f8475e
michaelshabib <63905853+michaelshabib@users.noreply.github.com>
2021-04-02 12:37:34 +0200

    FindMateCard

---
+++ lib/ui/mating/FindMateCard.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:petmatch/models/Pet.dart';
+import 'package:petmatch/theme/petsTheme.dart';
+import 'package:petmatch/widgets/Pair.dart';
+import 'package:petmatch/widgets/buttons/CircularMoreButton.dart';
+
+
+class FindMatingCard extends StatelessWidget {
+  FindMatingCard(this.pet);
+  final Pet pet;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                child: Image.network(pet.image, fit: BoxFit.cover,),
+                borderRadius: BorderRadius.circular(20),
+              )
+            ),
+            SizedBox(height: PetsTheme.getMeduimPadMarg(),),
+            ClipRRect(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:[
+                            Text(pet.name, style: TextStyle(fontSize: PetsTheme.getLargerFont(), fontWeight: FontWeight.bold, color: PetsTheme.blackTextColor),),
+                            Text("20 Miles away", style: TextStyle(fontSize: PetsTheme.getMeduimFont(), color: Colors.grey[600],)),
+                            ]
+                          ),
+                          CircularButton(
+                            child: SvgPicture.asset('assets/images/icons/mating/matingLove.svg'),
+                            radius: PetsTheme.radius3,
+                            backgroundColor: Colors.red[50], 
+                            onPressed: null
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: PetsTheme.getSmallPadMarg()),
+                      child: Text("View profile", style: TextStyle(fontSize: PetsTheme.getMeduimFont(), color:PetsTheme.currentMainColor, fontWeight: FontWeight.w500)),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 2, 
+                          child: Container(
+                            margin: EdgeInsets.only(right: PetsTheme.getSmallPadMarg()),
+                            decoration: BoxDecoration(
+                              color: PetsTheme.commentBgColor,
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: PetsTheme.radius2,
+                                    backgroundImage: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU").image,
+                                  ),
+                                  SizedBox(width: PetsTheme.getSmallPadMarg(),),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      FittedBox(child: Text("Owner", style: TextStyle(color: PetsTheme.currentMainColor),)),
+                                      FittedBox(child: FittedBox(child: Text(pet.owner),)),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ),
+                        Flexible(
+                          flex: 1, 
+                          child: Container(
+                            margin: EdgeInsets.all(PetsTheme.getSmallPadMarg()),
+                            decoration: BoxDecoration(
+                              color: PetsTheme.commentBgColor,
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Pair(
+                                FittedBox(child: Text("Breed",  style: TextStyle(color: PetsTheme.currentMainColor),)),
+                                FittedBox(child: Text("Golden")),
+                              ),
+                            ),
+                          )
+                        ),
+                        Flexible(
+                          flex: 1, 
+                          child: Container(
+                            margin: EdgeInsets.all(PetsTheme.getSmallPadMarg()),
+                            decoration: BoxDecoration(
+                              color: PetsTheme.commentBgColor,
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Pair(
+                                FittedBox(child: Text("Age", style: TextStyle(color: PetsTheme.currentMainColor),)),
+                                FittedBox(child: Text("6 months")),
+                              ),
+                            ),
+                          )
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Row(
+                      children: [
+                        SvgPicture.asset("assets/images/icons/paw.svg", color: PetsTheme.currentMainColor, width: 16,),
+                        SizedBox(width: PetsTheme.getSmallestPadMarg(),),
+                        RatingBar.builder(itemSize: PetsTheme.getSmallFont(),
+                          itemBuilder: (context, _) => Icon(Icons.star, color: PetsTheme.currentMainColor,),
+                          initialRating: 4.0,
+                          direction: Axis.horizontal,
+                          unratedColor: Colors.grey,
+                          allowHalfRating: true,
+                          onRatingUpdate: null
+                        ),
+                        Text("| 4.0", style: TextStyle(fontSize: PetsTheme.getSmallFont(), color: Colors.grey),),
+
+                        SizedBox(width: PetsTheme.getLargePadMarg(),),
+
+                        
+                        Icon(Icons.person, size: 20, color: PetsTheme.currentMainColor,),
+                        RatingBar.builder(itemSize: PetsTheme.getSmallFont(),
+                          itemBuilder: (context, _) => Icon(Icons.star, color: PetsTheme.currentMainColor,),
+                          initialRating: 3.0,
+                          direction: Axis.horizontal,
+                          unratedColor: Colors.grey,
+                          allowHalfRating: true,
+                          onRatingUpdate: null
+                        ),
+                        Text("| 3.0", style: TextStyle(fontSize: PetsTheme.getSmallFont(), color: Colors.grey),),
+
+                        Expanded(child: Container()),
+                        //reviews
+                        GestureDetector(
+                          child: Text("40 Reviews", style: TextStyle(color: PetsTheme.currentMainColor),),
+                        )
+                      ],
+                    )
+
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+  }
+}
--- lib/ui/mating/FindMateScreen.dart
+++ lib/ui/mating/FindMateScreen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:petmatch/models/Pet.dart';
 import 'package:petmatch/theme/petsTheme.dart';
-import 'package:petmatch/widgets/Pair.dart';
-import 'package:petmatch/widgets/buttons/CircularMoreButton.dart';
+import 'package:petmatch/ui/mating/FindMateCard.dart';
 import 'package:petmatch/widgets/screens/PetMatchSingleScreen.dart';
 
-
+List<Pet> pets = [
+    Pet("Coco", "https://images.indianexpress.com/2021/01/Golden-Retriever-dog.jpg", "Ahmed Mostafa"),
+    Pet("Bobby", "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg", "Mina"),
+    Pet("Lili", "https://communityimpact.com/uploads/images/2020/11/12/101807.JPG", "Aly"),
+];
 class FindMateScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -26,158 +28,12 @@ class FindMateScreen extends StatelessWidget {
               fontWeight: FontWeight.bold))
         ],
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-              child: ClipRRect(
-                child: Image.network("https://images.indianexpress.com/2021/01/Golden-Retriever-dog.jpg", fit: BoxFit.cover,),
-                borderRadius: BorderRadius.circular(20),
-              )
-            ),
-            SizedBox(height: PetsTheme.getMeduimPadMarg(),),
-            ClipRRect(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
-              child: Container(
-                color: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children:[
-                            Text("Coco", style: TextStyle(fontSize: PetsTheme.getLargerFont(), fontWeight: FontWeight.bold, color: PetsTheme.blackTextColor),),
-                            Text("20 Miles away", style: TextStyle(fontSize: PetsTheme.getMeduimFont(), color: Colors.grey[600],)),
-                            ]
-                          ),
-                          CircularButton(
-                            child: SvgPicture.asset('assets/images/icons/mating/matingLove.svg'),
-                            radius: PetsTheme.radius3,
-                            backgroundColor: Colors.red[50], 
-                            onPressed: null
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: PetsTheme.getSmallPadMarg()),
-                      child: Text("View profile", style: TextStyle(fontSize: PetsTheme.getMeduimFont(), color:PetsTheme.currentMainColor, fontWeight: FontWeight.w500)),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 2, 
-                          child: Container(
-                            margin: EdgeInsets.only(right: PetsTheme.getSmallPadMarg()),
-                            decoration: BoxDecoration(
-                              color: PetsTheme.commentBgColor,
-                              borderRadius: BorderRadius.circular(10)
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Row(
-                                children: [
-                                  CircleAvatar(
-                                    radius: PetsTheme.radius2,
-                                    backgroundImage: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU").image,
-                                  ),
-                                  SizedBox(width: PetsTheme.getSmallPadMarg(),),
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      FittedBox(child: Text("Owner", style: TextStyle(color: PetsTheme.currentMainColor),)),
-                                      FittedBox(child: Text("Ahmed Mostafa")),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          )
-                        ),
-                        Flexible(
-                          flex: 1, 
-                          child: Container(
-                            margin: EdgeInsets.all(PetsTheme.getSmallPadMarg()),
-                            decoration: BoxDecoration(
-                              color: PetsTheme.commentBgColor,
-                              borderRadius: BorderRadius.circular(10)
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Pair(
-                                FittedBox(child: Text("Breed",  style: TextStyle(color: PetsTheme.currentMainColor),)),
-                                FittedBox(child: Text("Golden")),
-                              ),
-                            ),
-                          )
-                        ),
-                        Flexible(
-                          flex: 1, 
-                          child: Container(
-                            margin: EdgeInsets.all(PetsTheme.getSmallPadMarg()),
-                            decoration: BoxDecoration(
-                              color: PetsTheme.commentBgColor,
-                              borderRadius: BorderRadius.circular(10)
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Pair(
-                                FittedBox(child: Text("Age", style: TextStyle(color: PetsTheme.currentMainColor),)),
-                                FittedBox(child: Text("6 months")),
-                              ),
-                            ),
-                          )
-                        ),
-                      ],
-                    ),
-                    Divider(),
-                    Row(
-                      children: [
-                        SvgPicture.asset("assets/images/icons/paw.svg", color: PetsTheme.currentMainColor, width: 16,),
-                        SizedBox(width: PetsTheme.getSmallestPadMarg(),),
-                        RatingBar.builder(itemSize: PetsTheme.getSmallFont(),
-                          itemBuilder: (context, _) => Icon(Icons.star, color: PetsTheme.currentMainColor,),
-                          initialRating: 4.0,
-                          direction: Axis.horizontal,
-                          unratedColor: Colors.grey,
-                          allowHalfRating: true,
-                          onRatingUpdate: null
-                        ),
-                        Text("| 4.0", style: TextStyle(fontSize: PetsTheme.getSmallFont(), color: Colors.grey),),
-
-                        SizedBox(width: PetsTheme.getLargePadMarg(),),
-
-                        
-                        Icon(Icons.person, size: 20, color: PetsTheme.currentMainColor,),
-                        RatingBar.builder(itemSize: PetsTheme.getSmallFont(),
-                          itemBuilder: (context, _) => Icon(Icons.star, color: PetsTheme.currentMainColor,),
-                          initialRating: 3.0,
-                          direction: Axis.horizontal,
-                          unratedColor: Colors.grey,
-                          allowHalfRating: true,
-                          onRatingUpdate: null
-                        ),
-                        Text("| 3.0", style: TextStyle(fontSize: PetsTheme.getSmallFont(), color: Colors.grey),),
-
-                        Expanded(child: Container()),
-                        //reviews
-                        GestureDetector(
-                          child: Text("40 Reviews", style: TextStyle(color: PetsTheme.currentMainColor),),
-                        )
-                      ],
-                    )
-
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+      //body: FindMatingCard(pets[0]),
+      body: PageView.builder(
+        itemCount: pets.length,
+        itemBuilder: (context, index) {
+          return FindMatingCard(pets[index]);
+        }
       ),
     );
   }
