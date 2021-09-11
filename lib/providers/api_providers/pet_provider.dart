3656dafc43b388a04a86df1e751aac6f7bdb5563
antoinmaged <antoin.abdelmalek@gmail.com>
2021-09-11 14:05:43 +0200

    Merge

--- lib/providers/api_providers/pet_provider.dart
+++ lib/providers/api_providers/pet_provider.dart
@@ -1,120 +1,120 @@
-import 'dart:async';
-import 'dart:convert';
+// import 'dart:async';
+// import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
-import 'package:petmatch/models/Pet.dart';
-import '../../models/Pet.dart';
-import 'package:http/http.dart' as http;
-import 'api_url.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:petmatch/models/Pet.dart';
+// import '../../models/Pet.dart';
+// import 'package:http/http.dart' as http;
+// import 'api_url.dart';
 
-class PetProvider extends ChangeNotifier {
-  //reset provider data
-  void reset() {
-    _isLoaded = null;
-    _companies = [];
-  }
+// class PetProvider extends ChangeNotifier {
+//   //reset provider data
+//   void reset() {
+//     _isLoaded = null;
+//     _companies = [];
+//   }
 
-  bool _isLoaded;
+//   bool _isLoaded;
 
-  bool get isLoaded => _isLoaded;
-  set isLoaded(bool isLoaded) {
-    _isLoaded = isLoaded;
-    notifyListeners();
-  }
+//   bool get isLoaded => _isLoaded;
+//   set isLoaded(bool isLoaded) {
+//     _isLoaded = isLoaded;
+//     notifyListeners();
+//   }
 
-  List<Pet> _companies = [];
-  List<Pet> get companies => _companies;
+//   List<Pet> _companies = [];
+//   List<Pet> get companies => _companies;
 
-  URLs _URLS = new URLs();
+//   URLs _URLS = new URLs();
 
-  Future<int> getAPet(String petID, {@required String token}) async {
-    print("start load <----");
-    var response;
-    try {
-      response = await http.get(
-        _URLS.getAPet + "?Pet_owner=$petID&uid=$petID&token=$token",
-      );
-    } catch (_) {
-      _isLoaded = false;
-      notifyListeners();
-      return -1;
-    }
-    print("Pet respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
-      List jsonList = jsonDecode(response.body);
-      _companies = jsonList.map((i) => Pet.fromJson(i)).toList();
-      print("done <----");
-      _isLoaded = true;
-      notifyListeners();
-      return 1;
-    } else {
-      // If that call was not successful, throw an error.
-      print("error");
-      _isLoaded = false;
-      notifyListeners();
-      return 0;
-    }
-  }
+//   Future<int> getAPet(String petID, {@required String token}) async {
+//     print("start load <----");
+//     var response;
+//     try {
+//       response = await http.get(
+//         _URLS.getAPet + "?Pet_owner=$petID&uid=$petID&token=$token",
+//       );
+//     } catch (_) {
+//       _isLoaded = false;
+//       notifyListeners();
+//       return -1;
+//     }
+//     print("Pet respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
+//       List jsonList = jsonDecode(response.body);
+//       _companies = jsonList.map((i) => Pet.fromJson(i)).toList();
+//       print("done <----");
+//       _isLoaded = true;
+//       notifyListeners();
+//       return 1;
+//     } else {
+//       // If that call was not successful, throw an error.
+//       print("error");
+//       _isLoaded = false;
+//       notifyListeners();
+//       return 0;
+//     }
+//   }
 
-  Future<List<Pet>> getAPetPet(String PetOwner,
-      {@required String petID, @required String token}) async {
-    print("start load <----");
-    var response;
+//   Future<List<Pet>> getAPetPet(String PetOwner,
+//       {@required String petID, @required String token}) async {
+//     print("start load <----");
+//     var response;
 
-    response = await http.get(
-      _URLS.getAPet + "?Pet_owner=$PetOwner&uid=$petID&token=$token",
-    );
+//     response = await http.get(
+//       _URLS.getAPet + "?Pet_owner=$PetOwner&uid=$petID&token=$token",
+//     );
 
-    print("Pet response <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
-      List jsonList = jsonDecode(response.body);
-      List<Pet> _petCompanies = jsonList.map((i) => Pet.fromJson(i)).toList();
-      print("done <----");
-      return _petCompanies;
-    } else {
-      // If that call was not successful, throw an error.
-      print("error");
-      throw ("response state code ${response.statusCode}");
-    }
-  }
+//     print("Pet response <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
+//       List jsonList = jsonDecode(response.body);
+//       List<Pet> _petCompanies = jsonList.map((i) => Pet.fromJson(i)).toList();
+//       print("done <----");
+//       return _petCompanies;
+//     } else {
+//       // If that call was not successful, throw an error.
+//       print("error");
+//       throw ("response state code ${response.statusCode}");
+//     }
+//   }
 
-  Future<int> createPet({Pet Pet, @required String token}) async {
-    print("start load <----");
-    var response;
-    try {
-      response = await http.get(_URLS.createPet +
-          "?title=${Pet.name}" +
-          "&image=${Pet.image}" +
-          "&bearer_token=$token");
-    } catch (_) {
-      _isLoaded = false;
-      notifyListeners();
-      return -1;
-    }
-    print("respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      Pet _newPet = Pet.fromJson(jsonDecode(response.body)[0]);
-      _companies.clear();
-      _companies.add(_newPet);
-      notifyListeners();
-      print("200 <----");
-      print("done <----");
-      return 1;
-    } else {
-      // If that call was not successful, throw an error.
-      print("error");
-      return 0;
-    }
-  }
-}
+//   Future<int> createPet({Pet Pet, @required String token}) async {
+//     print("start load <----");
+//     var response;
+//     try {
+//       response = await http.get(_URLS.createPet +
+//           "?title=${Pet.name}" +
+//           "&image=${Pet.image}" +
+//           "&bearer_token=$token");
+//     } catch (_) {
+//       _isLoaded = false;
+//       notifyListeners();
+//       return -1;
+//     }
+//     print("respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       Pet _newPet = Pet.fromJson(jsonDecode(response.body)[0]);
+//       _companies.clear();
+//       _companies.add(_newPet);
+//       notifyListeners();
+//       print("200 <----");
+//       print("done <----");
+//       return 1;
+//     } else {
+//       // If that call was not successful, throw an error.
+//       print("error");
+//       return 0;
+//     }
+//   }
+// }
--- lib/providers/api_providers/post_provider.dart
+++ lib/providers/api_providers/post_provider.dart
@@ -1,306 +1,306 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'api_url.dart';
-import '../../models/Post.dart';
+// import 'dart:async';
+// import 'dart:convert';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'api_url.dart';
+// import '../../models/Post.dart';
 
-class postProvider extends ChangeNotifier {
-  //reset provider data
-  void reset() {
-    _post = null;
-    _ispostLoaded = null;
-    _loading = false;
-  }
+// class postProvider extends ChangeNotifier {
+//   //reset provider data
+//   void reset() {
+//     _post = null;
+//     _ispostLoaded = null;
+//     _loading = false;
+//   }
 
-  Post _post;
-  Post get post => _post;
+//   Post _post;
+//   Post get post => _post;
 
-  URLs _URLS = new URLs();
+//   URLs _URLS = new URLs();
 
-  bool _loading = false;
-  bool get isLoaded => _loading;
-  set isLoaded(bool isLoaded) {
-    _loading = isLoaded;
-    notifyListeners();
-  }
+//   bool _loading = false;
+//   bool get isLoaded => _loading;
+//   set isLoaded(bool isLoaded) {
+//     _loading = isLoaded;
+//     notifyListeners();
+//   }
 
-  bool _ispostLoaded;
-  bool get ispostLoaded => _ispostLoaded;
-  set ispostLoaded(bool isLoaded) {
-    _ispostLoaded = isLoaded;
-    notifyListeners();
-  }
+//   bool _ispostLoaded;
+//   bool get ispostLoaded => _ispostLoaded;
+//   set ispostLoaded(bool isLoaded) {
+//     _ispostLoaded = isLoaded;
+//     notifyListeners();
+//   }
 
-// @required this.owner,
-//     @required this.postDate,
-//     @required this.commentsCount,
-//     @required this.sharesCount,
-//     @required this.lovesCount,
-//     @required isLoved,
-  Future<int> signUp(Post post, BuildContext context) async {
-    print("start load <----");
-    _loading = true;
-    notifyListeners();
-    String body;
-    body = "?user_id=${post.owner}" + "&post_id=${post.postDate}";
-    var response;
-    try {
-      response = await http.get(
-        _URLS.register + body,
-      );
-    } catch (_) {
-      print(_);
-      _loading = false;
-      notifyListeners();
-      return -1;
-    }
-    print("respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
+// // @required this.owner,
+// //     @required this.postDate,
+// //     @required this.commentsCount,
+// //     @required this.sharesCount,
+// //     @required this.lovesCount,
+// //     @required isLoved,
+//   Future<int> signUp(Post post, BuildContext context) async {
+//     print("start load <----");
+//     _loading = true;
+//     notifyListeners();
+//     String body;
+//     body = "?user_id=${post.owner}" + "&post_id=${post.postDate}";
+//     var response;
+//     try {
+//       response = await http.get(
+//         _URLS.register + body,
+//       );
+//     } catch (_) {
+//       print(_);
+//       _loading = false;
+//       notifyListeners();
+//       return -1;
+//     }
+//     print("respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
 
-      _post = Post.fromJson(json.decode(response.body));
+//       _post = Post.fromJson(json.decode(response.body));
 
-      _ispostLoaded = true;
-    }
-  }
+//       _ispostLoaded = true;
+//     }
+//   }
 
-  Future<int> login(
-      post post, BuildContext context, GlobalKey<ScaffoldState> scaffoldKey,
-      {bool socialMedia = false, String pageID}) async {
-    print("start load <----");
-    _loading = true;
-    notifyListeners();
-    http.Response response;
-    try {
-      response = await http.post(
-        _URLS.login,
-        body: {
-          "email": post.email,
-          "pass": post.password,
-        },
-      );
-    } catch (_) {
-      print(_);
-      _loading = false;
-      notifyListeners();
-      return -1;
-    }
+//   Future<int> login(
+//       post post, BuildContext context, GlobalKey<ScaffoldState> scaffoldKey,
+//       {bool socialMedia = false, String pageID}) async {
+//     print("start load <----");
+//     _loading = true;
+//     notifyListeners();
+//     http.Response response;
+//     try {
+//       response = await http.post(
+//         _URLS.login,
+//         body: {
+//           "email": post.email,
+//           "pass": post.password,
+//         },
+//       );
+//     } catch (_) {
+//       print(_);
+//       _loading = false;
+//       notifyListeners();
+//       return -1;
+//     }
 
-    print("response <----");
-    print(response.body);
-    print(response.statusCode);
-    print(response.headers);
-  }
+//     print("response <----");
+//     print(response.body);
+//     print(response.statusCode);
+//     print(response.headers);
+//   }
 
-  Future<int> getpostData({
-    @required String token,
-  }) async {
-    print("start load <----");
-    http.Response response;
-    try {
-      response = await http.get(
-        URLs.getpost + '&token=$token',
-      );
-    } catch (error) {
-      print(error);
-      _ispostLoaded = false;
-      notifyListeners();
-      return -1;
-    }
-    print("Jobbers respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
-      _post = post.fromJson(jsonDecode(response.body));
-      _ispostLoaded = true;
-      notifyListeners();
-      print("done <----");
-      return 1;
-    } else {
-      print("error ${response.statusCode}");
-      _ispostLoaded = false;
-      notifyListeners();
-      return 0;
-    }
-  }
+//   Future<int> getpostData({
+//     @required String token,
+//   }) async {
+//     print("start load <----");
+//     http.Response response;
+//     try {
+//       response = await http.get(
+//         URLs.getpost + '&token=$token',
+//       );
+//     } catch (error) {
+//       print(error);
+//       _ispostLoaded = false;
+//       notifyListeners();
+//       return -1;
+//     }
+//     print("Jobbers respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
+//       _post = post.fromJson(jsonDecode(response.body));
+//       _ispostLoaded = true;
+//       notifyListeners();
+//       print("done <----");
+//       return 1;
+//     } else {
+//       print("error ${response.statusCode}");
+//       _ispostLoaded = false;
+//       notifyListeners();
+//       return 0;
+//     }
+//   }
 
-  Future<int> updatePersonalInformation(post post) async {
-    print(_post.token);
-    print(_post.id);
-    String body = "?name=${post.name}" +
-        "&password=${post.password}" +
-        "&email=${post.email}" +
-        "&image=${post.image}" +
-        "&gender=${post.gender}" +
-        "&phone=${post.phone}" +
-        "&dateOfBirth=${post.dateOfBirth}" +
-        "&country=${post.country}" +
-        "&city=${post.city}" +
-        "&token=${post.token}" +
-        "&type=${post.posttype}";
-    if (post.password != null) {
-      body += "&pass=${post.password}";
-    }
+//   Future<int> updatePersonalInformation(post post) async {
+//     print(_post.token);
+//     print(_post.id);
+//     String body = "?name=${post.name}" +
+//         "&password=${post.password}" +
+//         "&email=${post.email}" +
+//         "&image=${post.image}" +
+//         "&gender=${post.gender}" +
+//         "&phone=${post.phone}" +
+//         "&dateOfBirth=${post.dateOfBirth}" +
+//         "&country=${post.country}" +
+//         "&city=${post.city}" +
+//         "&token=${post.token}" +
+//         "&type=${post.posttype}";
+//     if (post.password != null) {
+//       body += "&pass=${post.password}";
+//     }
 
-    if (post.name != _post.name) {
-      body += "&name=${post.name}";
-    }
-    print("start load <----");
-    _loading = true;
-    notifyListeners();
-    var response;
-    try {
-      response = await http.get(_URLS.updatepostData + body);
-    } catch (_) {
-      return -1;
-    }
-    print("respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
-      _post = post.fromJson(json.decode(response.body));
-      print("done <----");
-      return 1;
-    } else if (response.statusCode == 402) {
-      print("name exist");
-      return -2;
-    } else {
-      // If that call was not successful, throw an error.
-      print("error");
-      return 0;
-    }
-  }
+//     if (post.name != _post.name) {
+//       body += "&name=${post.name}";
+//     }
+//     print("start load <----");
+//     _loading = true;
+//     notifyListeners();
+//     var response;
+//     try {
+//       response = await http.get(_URLS.updatepostData + body);
+//     } catch (_) {
+//       return -1;
+//     }
+//     print("respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
+//       _post = post.fromJson(json.decode(response.body));
+//       print("done <----");
+//       return 1;
+//     } else if (response.statusCode == 402) {
+//       print("name exist");
+//       return -2;
+//     } else {
+//       // If that call was not successful, throw an error.
+//       print("error");
+//       return 0;
+//     }
+//   }
 
-  Future<int> updateAreaOfInterest(post post) async {
-    String body = "?name=${post.name}" +
-        "&password=${post.password}" +
-        "&email=${post.email}" +
-        "&image=${post.image}" +
-        "&gender=${post.gender}" +
-        "&phone=${post.phone}" +
-        "&dateOfBirth=${post.dateOfBirth}" +
-        "&country=${post.country}" +
-        "&city=${post.city}" +
-        "&token=${post.token}" +
-        "&type=${post.posttype}";
-    print("start load <----");
-    _loading = true;
-    notifyListeners();
-    var response;
-    try {
-      response = await http.get(_URLS.updatepostData + body);
-    } catch (_) {
-      _loading = false;
-      notifyListeners();
-      return -1;
-    }
-    print("respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
+//   Future<int> updateAreaOfInterest(post post) async {
+//     String body = "?name=${post.name}" +
+//         "&password=${post.password}" +
+//         "&email=${post.email}" +
+//         "&image=${post.image}" +
+//         "&gender=${post.gender}" +
+//         "&phone=${post.phone}" +
+//         "&dateOfBirth=${post.dateOfBirth}" +
+//         "&country=${post.country}" +
+//         "&city=${post.city}" +
+//         "&token=${post.token}" +
+//         "&type=${post.posttype}";
+//     print("start load <----");
+//     _loading = true;
+//     notifyListeners();
+//     var response;
+//     try {
+//       response = await http.get(_URLS.updatepostData + body);
+//     } catch (_) {
+//       _loading = false;
+//       notifyListeners();
+//       return -1;
+//     }
+//     print("respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
 
-      _loading = false;
-      notifyListeners();
-      print("done <----");
-      return 1;
-    } else if (response.statusCode == 401) {
-      _loading = false;
-      notifyListeners();
-      print("email exist");
-      return -2;
-    } else {
-      // If that call was not successful, throw an error.
-      _loading = false;
-      notifyListeners();
-      print("error");
-      return 0;
-    }
-  }
+//       _loading = false;
+//       notifyListeners();
+//       print("done <----");
+//       return 1;
+//     } else if (response.statusCode == 401) {
+//       _loading = false;
+//       notifyListeners();
+//       print("email exist");
+//       return -2;
+//     } else {
+//       // If that call was not successful, throw an error.
+//       _loading = false;
+//       notifyListeners();
+//       print("error");
+//       return 0;
+//     }
+//   }
 
-  Future<int> updateSocialMediaLinks(post post) async {
-    String body = "?name=${post.name}" +
-        "&password=${post.password}" +
-        "&email=${post.email}" +
-        "&image=${post.image}" +
-        "&gender=${post.gender}" +
-        "&phone=${post.phone}" +
-        "&dateOfBirth=${post.dateOfBirth}" +
-        "&country=${post.country}" +
-        "&city=${post.city}" +
-        "&token=${post.token}" +
-        "&type=${post.posttype}";
-    print("start load <----");
-    _loading = true;
-    notifyListeners();
-    var response;
-    try {
-      response = await http.get(_URLS.updatepostData + body);
-    } catch (_) {
-      _loading = false;
-      notifyListeners();
-      return -1;
-    }
-    print("respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
-      _post = post.fromJson(json.decode(response.body));
-      _loading = false;
-      notifyListeners();
-      print("done <----");
-      return 1;
-    } else if (response.statusCode == 401) {
-      _loading = false;
-      notifyListeners();
-      print("email exist");
-      return -2;
-    } else {
-      // If that call was not successful, throw an error.
-      _loading = false;
-      notifyListeners();
-      print("error");
-      return 0;
-    }
-  }
+//   Future<int> updateSocialMediaLinks(post post) async {
+//     String body = "?name=${post.name}" +
+//         "&password=${post.password}" +
+//         "&email=${post.email}" +
+//         "&image=${post.image}" +
+//         "&gender=${post.gender}" +
+//         "&phone=${post.phone}" +
+//         "&dateOfBirth=${post.dateOfBirth}" +
+//         "&country=${post.country}" +
+//         "&city=${post.city}" +
+//         "&token=${post.token}" +
+//         "&type=${post.posttype}";
+//     print("start load <----");
+//     _loading = true;
+//     notifyListeners();
+//     var response;
+//     try {
+//       response = await http.get(_URLS.updatepostData + body);
+//     } catch (_) {
+//       _loading = false;
+//       notifyListeners();
+//       return -1;
+//     }
+//     print("respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
+//       _post = post.fromJson(json.decode(response.body));
+//       _loading = false;
+//       notifyListeners();
+//       print("done <----");
+//       return 1;
+//     } else if (response.statusCode == 401) {
+//       _loading = false;
+//       notifyListeners();
+//       print("email exist");
+//       return -2;
+//     } else {
+//       // If that call was not successful, throw an error.
+//       _loading = false;
+//       notifyListeners();
+//       print("error");
+//       return 0;
+//     }
+//   }
 
-  Future<int> updateProfileImage(post post) async {
-    print("start load <----");
-    var response;
-    try {
-      response = await http.post(URLs.editProfileImage,
-          body: {"uid": _post.id, "image": post.image, "token": _post.token});
-    } catch (error) {
-      print(error);
-      return -1;
-    }
+//   Future<int> updateProfileImage(post post) async {
+//     print("start load <----");
+//     var response;
+//     try {
+//       response = await http.post(URLs.editProfileImage,
+//           body: {"uid": _post.id, "image": post.image, "token": _post.token});
+//     } catch (error) {
+//       print(error);
+//       return -1;
+//     }
 
-    print("respones <----");
-    print(response.body);
-    // If the call to the server was successful, parse the JSON.
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      // If the call to the server was successful, parse the JSON.
-      print("200 <----");
-      _post.image = post.fromJson(json.decode(response.body)).image;
-      notifyListeners();
-      print("done <----");
-      return 1;
-    } else {
-      // If that call was not successful, throw an error.
-      print("error");
-      return 0;
-    }
-  }
-}
+//     print("respones <----");
+//     print(response.body);
+//     // If the call to the server was successful, parse the JSON.
+//     if (response.statusCode >= 200 && response.statusCode < 300) {
+//       // If the call to the server was successful, parse the JSON.
+//       print("200 <----");
+//       _post.image = post.fromJson(json.decode(response.body)).image;
+//       notifyListeners();
+//       print("done <----");
+//       return 1;
+//     } else {
+//       // If that call was not successful, throw an error.
+//       print("error");
+//       return 0;
+//     }
+//   }
+// }
